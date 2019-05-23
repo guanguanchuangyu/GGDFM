981f96f15033957a611fd7b7de78f0e77e25f86c
qiang dai <guanguanchangyu@outlook.com>
2019-05-24 00:56:32 +0800

    ggcy-添加首页部分显示

---
+++ GGCYUI/Controls/SampleGroup.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GGCYUI.Controls
+{
+    /// <summary>
+    /// 简单的内容组容器
+    /// </summary>
+    public class SampleGroup : ItemsControl
+    {
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new ContentControl();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is ContentControl;
+        }
+
+        #region Orientation
+        public Orientation Orientation
+        {
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register("Orientation", typeof(Orientation), typeof(SampleGroup), new PropertyMetadata(default(Orientation)));
+        #endregion
+        #region ItemStyleSelector
+        public StyleSelector ItemStyleSelector
+        {
+            get { return (StyleSelector)GetValue(ItemStyleSelectorProperty); }
+            set { SetValue(ItemStyleSelectorProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemStyleSelectorProperty =
+            DependencyProperty.Register("ItemStyleSelector", typeof(StyleSelector), typeof(SampleGroup), new PropertyMetadata(default(StyleSelector))); 
+        #endregion
+
+        protected override void OnVisualChildrenChanged(DependencyObject visualAdded, DependencyObject visualRemoved)
+        {
+            base.OnVisualChildrenChanged(visualAdded, visualRemoved);
+
+            var count = Items.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var item = (ContentControl)Items[i];
+                item.Style = ItemStyleSelector?.SelectStyle(item, this);
+            }
+        }
+
+    }
+}
